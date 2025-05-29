b395df24b6ccd81007f0f223750521869cd0cf61
fbmcarmo <fbrunomcarmo@gmail.com>
2025-05-29 11:59:23 -0300

    Adicionei a conexão com o postgres

--- index.js
+++ index.js
@@ -1,47 +1,63 @@
 const express = require('express')
 const app = express()
 const port = 6579
-const produtosDb = []
-const usersDb = []
+const { Pool } = require('pg')
+
+const pool = new Pool({
+    user: 'postgres.quhistxkaykogntsjsme',
+    host: 'aws-0-sa-east-1.pooler.supabase.com',
+    database: 'postgres',
+    password: 'Bruno0moreira020289',
+    port: 5432
+})
 
 app.use(express.json())
 
-app.get("/", (req, res) => {
-    res.send("Olá Bruno")
-})
+app.post('/produtos', async (req, res) => {
+    const { nome, preco, categoria, image_url } = req.body
 
-app.post("/produtos", (req, res) => {
-    const {nome, preco, categoria} = req.body
-
-    if(!nome || !preco || !categoria){
-        return res.status(400).send("preco, nome e categoria são obrigatórios")
+    if(!nome || !preco || !categoria || !image_url){
+        return res.status(400).send('Todos os campos são obrigatórios')
     }
 
-    const produto = {
-        nome: nome,
-        preco: preco,
-        categoria: categoria
+    if(nome.length > 100){
+        return res.status(400).send('Nome pode ter no máximo 100 caracteres')
     }
 
-    produtosDb.push(produto)
-
-    res.status(201).send(produto)
-})
-
-app.get("/produtos", (req, res) => {
-    res.send(produtosDb)
-})
-
-app.post("/usuarios", (req, res) => {
-    const {nome, email, senha} = req.body
-
-    if(!nome || !email || !senha){
-        return res.status(400).send("email, nome e senha são obrigatórios")
+    if(categoria.length > 50){
+        return res.status(400).send('Categoria pode ter no máximo 50 caracteres')
     }
 
-    usersDb.push({nome, email, senha})
+    try {
+        const produto = await pool.query(`
+            INSERT INTO produtos (nome, preco, categoria, image_url)
+            VALUES (
+                '${nome}',
+                ${preco},
+                '${categoria}',
+                '${image_url}'
+            )
+            RETURNING *
+        `)
 
-    res.status(201).send("Usuário cadastrado com sucesso!")
+        res.status(201).send(produto.rows[0])
+        
+    } catch (error) {
+        console.error(error)
+        res.status(500).send('Erro ao cadastrar produto')
+    }
+})
+
+app.get('/produtos', async (req, res) => {
+    try {
+        const produtos = await pool.query('SELECT * FROM produtos')
+
+        return res.status(200).send(produtos.rows)
+        
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send('Erro ao buscar produtos')
+    }
 })
 
 app.listen(port, () => {
